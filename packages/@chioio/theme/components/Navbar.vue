e4e5f02c97d93ac3720e18ce7ea3b28a4c271b81
Tenn Chio <Tenn_Chio@icloud.com>
2020-12-23 22:58:32 +0800

    navbar

---
+++ packages/@chioio/theme/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="navbar">
+    
+    <ThemeSwitcher />
+  </header>
+</template>
+
+<script>
+import ThemeSwitcher from '@theme/components/ThemeSwitcher.vue'
+
+export default {
+  name: 'Navbar',
+  components: {
+    ThemeSwitcher
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+
+.navbar {
+  height: $navbarHeight;
+  background: var(--navbar-background);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+<style lang="scss">
+html[data-theme='light'] {
+  --navbar-background: #efefef;
+}
+
+html[data-theme='dark'] {
+  --navbar-background: #303030
+}
+</style>
